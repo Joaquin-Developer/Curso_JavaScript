d455c87e3c31e45593c44d6ffbac479feb862cee
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-12-01 18:09:23 -0300

    realizo práctica 4

---
+++ importarJSON/App.js
@@ -0,0 +1,11 @@
+
+// import * as data from "./data.json";
+
+
+const datos = require("./data.json");
+
+datos.forEach(elem => {
+
+    console.log(elem.name);
+    
+});
--- nivelJunior/Cap4_MetodosDeCadenas/Practico4/App.js
+++ nivelJunior/Cap4_MetodosDeCadenas/Practico4/App.js
@@ -1,8 +1,47 @@
+/**
+ * Implementar una funcion que retorne el profesor asignado a una materia, y nombre de todos los alumnos 
+ * de la misma. Indicar en cuantas clases esta inscripto cofla (Indicar nombre de clase y profesor a cargo)
+ */
 
-class Curso {
+class Materia {
 
-    constructor() {
-        
+    constructor(nombre, profesor, alumnos) {
+        this.nombre = nombre;
+        this.profesor = profesor;
+        this.alumnos = alumnos;
+    }
+
+    pertenece(nombreAlumno) {
+        for (const elem of this.alumnos) {
+            if (elem === nombreAlumno)
+                return true;
+        }
+        return false;
+    }
+
+    inscribir(nombreAlumno) {
+        if (this.alumnos.length >= 6) {
+            alert(`No se pudo inscribir a la materia ${this.nombre}, debido a que excede el aforo.`);
+        } else if (this.pertenece(nombreAlumno)) {
+            alert(`El alumno ${nombreAlumno} ya se encuentra inscripto en el curso ${this.nombre}`);
+        } else {
+            this.alumnos.push(nombreAlumno);
+            alert(`Se iscribió al alumno ${nombreAlumno} al curso ${this.nombre} con el Prof. ${this.profesor}`);
+        }
+    }
+
+    recorrer() {
+        console.log("Lista de Alumnos: " + this.alumnos.join(", "));
     }
 
 }
+
+const programacion = new Materia("P1", "Joaquín", ["Pepe", "Juan", "Jose"]);
+programacion.inscribir("Cofla");
+programacion.recorrer();
+
+document.getElementById("btnAgregar").addEventListener("click", function() {
+    programacion.inscribir(document.getElementById("nombre").value);
+    console.log(programacion.alumnos.join(","));
+
+});
