8d73cb081b22e2735175eff6ebe294ab5dfbb958
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-12-10 15:26:32 -0300

    finalizo cap.7

---
+++ nivelMedio/Cap7_Window/App.js
@@ -0,0 +1,23 @@
+/**
+ * Ejercicio del cap. 7 
+ */
+
+// <b></b>
+const html = `
+    Protocolo: <b>${window.location.protocol}</b><br>
+    Hostname: <b>${window.location.hostname}</b><br>
+    Pathname: <b>${window.location.pathname}</b><br>
+    URL Completa: <b>${window.location.href}</b><br>
+    
+`;
+
+document.write(html);
+
+// const height = window.screen.height;
+// const widht = window.screen.width;
+
+// const userOption = confirm("El alto es " + height + " y el ancho es " + widht);
+
+// if (userOption) alert("Compra realizada correctamente"); else alert("Compra no realizada");
+
+
